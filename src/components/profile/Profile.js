2c41116f2bf2aaa48d71ce3feaccfa77d362b703
Brandon <brandon121j@gmail.com>
2021-12-12 00:54:07 -0500

    Profile.js now reflects users first and last name

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,11 +1,12 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import jwtDecode from 'jwt-decode';
 
 import { AuthContext } from '../../context/AuthContext';
 
 function Profile() {
-	const { state: user, dispatch } = useContext(AuthContext);
-	
+	const { state: email, dispatch } = useContext(AuthContext);
+	const [data, setData] = useState([]);
 
 
 	const onClickHandler = () => {
@@ -15,11 +16,19 @@ function Profile() {
 		window.localStorage.removeItem('jwtToken');
 	};
 
+	useEffect(() => {
+		let payload = window.localStorage.getItem("jwtToken");
+		let decodedToken = jwtDecode(payload);
+		setData(decodedToken)
+	
+	}, [])
+
 	return (
 		// <div>{user.user.firstName}</div>
 		<div className='d-flex justify-content-center'>
 		<div className='d-flex justify-content-center flex-column'>
-			<h1>Profile</h1>
+			<h1>{data.firstName} {data.lastName}</h1>
+			
 			<button className="btn btn-info" onClick={onClickHandler}>Log out</button>
 			</div>
 		</div>
