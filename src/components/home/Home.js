c2a8b36ce75cd1b019775c78870e871089ba50eb
Brandon <brandon121j@gmail.com>
2021-12-12 23:47:04 -0500

    CSS Changes

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import CheckToken from '../../hooks/CheckToken';
 
-
 function Home() {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
+	return (
+		<div className="d-flex justify-content-center text-center rounded m-5">
+			<h1>Home</h1>
+			<div className="card">
+					<h3>Body</h3>
+			</div>
+		</div>
+	);
 }
 
 export default Home;
