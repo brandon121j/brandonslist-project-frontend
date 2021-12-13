b7172712f832fbd16017daa3c66dc475bb90ee99
Brandon <brandon121j@gmail.com>
2021-12-12 23:52:34 -0500

    Basic CSS setup for the followup Backend data

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,12 +4,19 @@ import CheckToken from '../../hooks/CheckToken';
 
 function Home() {
 	return (
-		<div className="d-flex justify-content-center text-center rounded m-5">
+        <>
+        <div className="d-flex justify-content-center text-center rounded m-5">
 			<h1>Home</h1>
-			<div className="card">
-					<h3>Body</h3>
+            </div>
+		<div className="d-flex justify-content-center text-center rounded m-5 p-3">
+			<div className="card w-25">
+					<h3>Posting Title</h3>
+                    <div className='card-body'>
+                        <p>Posting Content</p>
+                    </div>
 			</div>
 		</div>
+        </>
 	);
 }
 
