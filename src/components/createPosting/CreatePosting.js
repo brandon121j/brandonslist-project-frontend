11569b1be06880488a92b276db1edcb66929d4df
Brandon <brandon121j@gmail.com>
2021-12-13 13:13:19 -0500

    Basic setup for CreatePosting.js

--- src/components/createPosting/CreatePosting.js
+++ src/components/createPosting/CreatePosting.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 const CreatePosting = () => {
     return (
-        <div>
-            Create Posting
+        <div className='d-flex justify-content-center text-center rounded m-5'>
+            <div className="card w-25">
+            <form class="form-group card-body">
+                
+            </form>
+            </div>
         </div>
     )
 }
